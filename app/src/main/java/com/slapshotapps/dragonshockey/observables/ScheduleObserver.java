3a43124e3a9a113ff988137c2d11416742927553
Will Metz <willmetz@gmail.com>
2016-06-29 21:48:37 -0400

    changed the code style and also implemented logic to drop the firebase reference when the subscription is done

--- app/src/main/java/com/slapshotapps/dragonshockey/observables/ScheduleObserver.java
+++ app/src/main/java/com/slapshotapps/dragonshockey/observables/ScheduleObserver.java
@@ -19,123 +19,159 @@ import java.util.Date;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.Subscription;
+import rx.functions.Action0;
 import rx.functions.Action1;
 import rx.functions.Func1;
+import rx.subscriptions.Subscriptions;
 
 /**
  * A custom class to help with observable code.
  */
 public class ScheduleObserver {
-  public static Observable<SeasonSchedule> getHockeySchedule(
-      final FirebaseDatabase firebaseDatabase) {
-    return Observable.create(new Observable.OnSubscribe<SeasonSchedule>() {
-      @Override public void call(final Subscriber<? super SeasonSchedule> subscriber) {
-        Query query = firebaseDatabase.getReference(Config.GAMES).orderByChild("gameID");
+    public static Observable<SeasonSchedule> getHockeySchedule(
+            final FirebaseDatabase firebaseDatabase) {
+        return Observable.create(new Observable.OnSubscribe<SeasonSchedule>() {
+            @Override
+            public void call(final Subscriber<? super SeasonSchedule> subscriber) {
+                final Query query = firebaseDatabase.getReference(Config.GAMES).orderByChild("gameID");
 
-        query.addValueEventListener(new ValueEventListener() {
-          @Override public void onDataChange(DataSnapshot dataSnapshot) {
-            SeasonSchedule schedule = new SeasonSchedule();
+                final ValueEventListener listener = query.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        SeasonSchedule schedule = new SeasonSchedule();
 
-            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-              schedule.addGame(snapshot.getValue(Game.class));
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            schedule.addGame(snapshot.getValue(Game.class));
+                        }
+
+                        if (!subscriber.isUnsubscribed()) {
+                            subscriber.onNext(schedule);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.e("tag", "Error retrieving data");
+                    }
+                });
+
+                //when the subscription is cancelled remove the listener
+                subscriber.add(Subscriptions.create(new Action0() {
+                    @Override
+                    public void call() {
+                        query.removeEventListener(listener);
+                    }
+                }));
             }
-
-            if (!subscriber.isUnsubscribed()) {
-              subscriber.onNext(schedule);
-            }
-          }
-
-          @Override public void onCancelled(DatabaseError databaseError) {
-            Log.e("tag", "Error retrieving data");
-          }
         });
-      }
-    });
-  }
+    }
 
-  public static Observable<HomeContents> getHomeScreen(final FirebaseDatabase firebaseDatabase,
-      final SeasonSchedule schedule, final Date referenceDate) {
-    return Observable.create(new Observable.OnSubscribe<HomeContents>() {
-      @Override public void call(final Subscriber<? super HomeContents> subscriber) {
+    public static Observable<HomeContents> getHomeScreen(final FirebaseDatabase firebaseDatabase,
+                                                         final SeasonSchedule schedule, final Date referenceDate) {
+        return Observable.create(new Observable.OnSubscribe<HomeContents>() {
+            @Override
+            public void call(final Subscriber<? super HomeContents> subscriber) {
 
-        //find the next game and last game IDs
-        final Game nextGame = ScheduleUtils.getGameAfterDate(referenceDate, schedule.getAllGames());
-        final Game lastGame =
-            ScheduleUtils.getGameBeforeDate(referenceDate, schedule.getAllGames());
+                //find the next game and last game IDs
+                final Game nextGame = ScheduleUtils.getGameAfterDate(referenceDate, schedule.getAllGames());
+                final Game lastGame =
+                        ScheduleUtils.getGameBeforeDate(referenceDate, schedule.getAllGames());
 
-        Query query = firebaseDatabase.getReference(Config.GAME_RESULTS).orderByChild("gameID");
-        if (nextGame != null && lastGame != null) {
-          query.endAt(nextGame.gameID).startAt(lastGame.gameID);
-        } else if (nextGame != null) {
-          query.equalTo(nextGame.gameID);
-        } else if (lastGame != null) {
-          query.equalTo(lastGame.gameID);
-        } else {
-          subscriber.onError(new Throwable("No games found"));
-        }
-
-        query.addValueEventListener(new ValueEventListener() {
-          @Override public void onDataChange(DataSnapshot dataSnapshot) {
-
-            HomeContents homeContents = new HomeContents();
-            homeContents.lastGame = lastGame;
-            homeContents.nextGame = nextGame;
-
-            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-
-              GameResult gameResult = snapshot.getValue(GameResult.class);
-
-              if (lastGame != null && lastGame.gameID == gameResult.gameID) {
-                homeContents.lastGame.gameResult = gameResult;
-              } else if (nextGame != null && nextGame.gameID == gameResult.gameID) {
-                homeContents.nextGame.gameResult = gameResult;
-              }
-            }
-
-            if (!subscriber.isUnsubscribed()) {
-              subscriber.onNext(homeContents);
-            }
-          }
-
-          @Override public void onCancelled(DatabaseError databaseError) {
-
-          }
-        });
-      }
-    });
-  }
-
-  public static Observable<SeasonSchedule> getScheduleWithResults(
-      final FirebaseDatabase firebaseDatabase, final SeasonSchedule schedule) {
-    return Observable.create(new Observable.OnSubscribe<SeasonSchedule>() {
-      @Override public void call(final Subscriber<? super SeasonSchedule> subscriber) {
-        Query query = firebaseDatabase.getReference(Config.GAME_RESULTS).orderByChild("gameID");
-
-        query.addValueEventListener(new ValueEventListener() {
-          @Override public void onDataChange(DataSnapshot dataSnapshot) {
-
-            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-              GameResult gameResult = snapshot.getValue(GameResult.class);
-
-              if (gameResult != null) {
-                Game game = schedule.getGame(gameResult.gameID);
-
-                if (game != null) {
-                  game.gameResult = gameResult;
+                final Query query = firebaseDatabase.getReference(Config.GAME_RESULTS).orderByChild("gameID");
+                if (nextGame != null && lastGame != null) {
+                    query.endAt(nextGame.gameID).startAt(lastGame.gameID);
+                } else if (nextGame != null) {
+                    query.equalTo(nextGame.gameID);
+                } else if (lastGame != null) {
+                    query.equalTo(lastGame.gameID);
+                } else {
+                    subscriber.onError(new Throwable("No games found"));
                 }
-              }
+
+                final ValueEventListener listener = query.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        HomeContents homeContents = new HomeContents();
+                        homeContents.lastGame = lastGame;
+                        homeContents.nextGame = nextGame;
+
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                            GameResult gameResult = snapshot.getValue(GameResult.class);
+
+                            if (lastGame != null && lastGame.gameID == gameResult.gameID) {
+                                homeContents.lastGame.gameResult = gameResult;
+                            } else if (nextGame != null && nextGame.gameID == gameResult.gameID) {
+                                homeContents.nextGame.gameResult = gameResult;
+                            }
+                        }
+
+                        if (!subscriber.isUnsubscribed()) {
+                            subscriber.onNext(homeContents);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                //when the subscription is cancelled remove the listener
+                subscriber.add(Subscriptions.create(new Action0() {
+                    @Override
+                    public void call() {
+                        query.removeEventListener(listener);
+                    }
+                }));
             }
-
-            if (!subscriber.isUnsubscribed()) {
-              subscriber.onNext(schedule);
-            }
-          }
-
-          @Override public void onCancelled(DatabaseError databaseError) {
-
-          }
         });
-      }
-    });
-  }
+    }
+
+    public static Observable<SeasonSchedule> getScheduleWithResults(
+            final FirebaseDatabase firebaseDatabase, final SeasonSchedule schedule) {
+        return Observable.create(new Observable.OnSubscribe<SeasonSchedule>() {
+            @Override
+            public void call(final Subscriber<? super SeasonSchedule> subscriber) {
+                final Query query = firebaseDatabase.getReference(Config.GAME_RESULTS).orderByChild("gameID");
+
+                final ValueEventListener listener = query.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            GameResult gameResult = snapshot.getValue(GameResult.class);
+
+                            if (gameResult != null) {
+                                Game game = schedule.getGame(gameResult.gameID);
+
+                                if (game != null) {
+                                    game.gameResult = gameResult;
+                                }
+                            }
+                        }
+
+                        if (!subscriber.isUnsubscribed()) {
+                            subscriber.onNext(schedule);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                //when the subscription is cancelled remove the listener
+                subscriber.add(Subscriptions.create(new Action0() {
+                    @Override
+                    public void call() {
+                        query.removeEventListener(listener);
+                    }
+                }));
+            }
+        });
+    }
 }
