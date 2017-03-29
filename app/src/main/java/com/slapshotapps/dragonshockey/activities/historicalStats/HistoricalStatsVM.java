7df86f03bc53466b63b23dc8f55d22cd914899df
Will Metz <willmetz@gmail.com>
2017-03-28 22:08:54 -0400

    starting work on filtering the stats

--- app/src/main/java/com/slapshotapps/dragonshockey/activities/historicalStats/HistoricalStatsVM.java
+++ app/src/main/java/com/slapshotapps/dragonshockey/activities/historicalStats/HistoricalStatsVM.java
@@ -3,28 +3,60 @@ package com.slapshotapps.dragonshockey.activities.historicalStats;
 
 import com.android.annotations.NonNull;
 import com.slapshotapps.dragonshockey.Utils.RosterUtils;
+import com.slapshotapps.dragonshockey.models.GameStats;
 import com.slapshotapps.dragonshockey.models.Player;
+import com.slapshotapps.dragonshockey.models.PlayerHistoricalStats;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class HistoricalStatsVM {
 
     private final Player player;
+    private List<PlayerHistoricalStats> playerHistoricalStats;
 
-    public HistoricalStatsVM(@NonNull Player player){
+    public HistoricalStatsVM(@NonNull Player player, GameStats currentSeasonStats, List<PlayerHistoricalStats> historicalStats) {
         this.player = player;
+        this.playerHistoricalStats = new ArrayList<>();
     }
 
-    public String getPlayerName(){
+    public String getPlayerName() {
         return RosterUtils.getFullName(player);
     }
 
-    public String getPlayerNumber(){
+    public String getPlayerNumber() {
         return String.format(Locale.US, "# %s", RosterUtils.getNumber(player));
     }
 
-    public String getPlayerPosition(){
+    public String getPlayerPosition() {
         return RosterUtils.getPosition(player);
+    }
+
+    public List<PlayerHistoricalStats> getStats() {
+        return playerHistoricalStats;
+    }
+
+    private void filterStatsForPlayer(PlayerHistoricalStats unfilteredStats) {
+        for(GameStats gameStats : unfilteredStats.games){
+            
+        }
+    }
+
+    private void addCurrentSeason(GameStats currentSeasonStats) {
+        PlayerHistoricalStats currentSeasonFilteredStats = new PlayerHistoricalStats();
+        currentSeasonFilteredStats.seasonID = "Current";
+        currentSeasonFilteredStats.games = new ArrayList<>(1);
+        GameStats filteredForPlayer = new GameStats();
+        filteredForPlayer.gameStats = new ArrayList<>();
+
+        for (GameStats.Stats stats : currentSeasonStats.gameStats) {
+            if (stats.playerID == player.playerID) {
+                filteredForPlayer.gameStats.add(stats);
+            }
+        }
+
+        playerHistoricalStats.add(currentSeasonFilteredStats);
     }
 
 }
