6e44fcb80055a94b11cdf624fa2c75df72d086a6
Will Metz <willmetz@gmail.com>
2017-11-03 20:16:20 -0400

    completed sorting test

--- app/src/test/java/com/slapshotapps/dragonshockey/activities/stats/adapters/PlayerStatsVMTest.java
+++ app/src/test/java/com/slapshotapps/dragonshockey/activities/stats/adapters/PlayerStatsVMTest.java
@@ -119,4 +119,193 @@ public class PlayerStatsVMTest {
         Assert.assertEquals("1", playerStats.get(4).getAssists());
         Assert.assertEquals("zaon", playerStats.get(4).getLastName());
     }
+
+    @Test
+    public void testSortPoints() {
+
+        ArrayList<PlayerStatsVM> playerStats = new ArrayList<>();
+
+        PlayerStats stats = new PlayerStats(1, "bob", "joe");
+        stats.assists = 12;
+        stats.points = 33;
+        PlayerStatsVM viewModel = new PlayerStatsVM(stats, null);
+        viewModel.setSortSelection(StatsSortDialogFragment.StatSortSelection.Points);
+        playerStats.add(viewModel);
+
+        stats = new PlayerStats(1, "jeff", "france");
+        stats.assists = 5;
+        stats.points = 22;
+        viewModel = new PlayerStatsVM(stats, null);
+        viewModel.setSortSelection(StatsSortDialogFragment.StatSortSelection.Points);
+        playerStats.add(viewModel);
+
+        stats = new PlayerStats(1, "dan", "wham");
+        stats.assists = 2;
+        stats.points = 18;
+        viewModel = new PlayerStatsVM(stats, null);
+        viewModel.setSortSelection(StatsSortDialogFragment.StatSortSelection.Points);
+        playerStats.add(viewModel);
+
+        stats = new PlayerStats(1, "will", "zaon");
+        stats.assists = 1;
+        stats.points = 22;
+        viewModel = new PlayerStatsVM(stats, null);
+        viewModel.setSortSelection(StatsSortDialogFragment.StatSortSelection.Points);
+        playerStats.add(viewModel);
+
+        stats = new PlayerStats(1, "bob", "aa");
+        stats.assists = 2;
+        stats.points = 3;
+        viewModel = new PlayerStatsVM(stats, null);
+        viewModel.setSortSelection(StatsSortDialogFragment.StatSortSelection.Points);
+        playerStats.add(viewModel);
+
+        Collections.sort(playerStats);
+
+        Assert.assertEquals("33", playerStats.get(0).getPoints());
+        Assert.assertEquals("joe", playerStats.get(0).getLastName());
+
+        Assert.assertEquals("22", playerStats.get(1).getPoints());
+        Assert.assertEquals("france", playerStats.get(1).getLastName());
+
+        Assert.assertEquals("22", playerStats.get(2).getPoints());
+        Assert.assertEquals("zaon", playerStats.get(2).getLastName());
+
+        Assert.assertEquals("18", playerStats.get(3).getPoints());
+        Assert.assertEquals("wham", playerStats.get(3).getLastName());
+
+        Assert.assertEquals("3", playerStats.get(4).getPoints());
+        Assert.assertEquals("aa", playerStats.get(4).getLastName());
+    }
+
+    @Test
+    public void testSortPenaltyMinutes() {
+
+        ArrayList<PlayerStatsVM> playerStats = new ArrayList<>();
+
+        PlayerStats stats = new PlayerStats(1, "bob", "joe");
+        stats.assists = 12;
+        stats.points = 33;
+        stats.penaltyMinutes = 2;
+        PlayerStatsVM viewModel = new PlayerStatsVM(stats, null);
+        viewModel.setSortSelection(StatsSortDialogFragment.StatSortSelection.PenaltyMinutes);
+        playerStats.add(viewModel);
+
+        stats = new PlayerStats(1, "jeff", "france");
+        stats.assists = 5;
+        stats.points = 22;
+        stats.penaltyMinutes = 0;
+        viewModel = new PlayerStatsVM(stats, null);
+        viewModel.setSortSelection(StatsSortDialogFragment.StatSortSelection.PenaltyMinutes);
+        playerStats.add(viewModel);
+
+        stats = new PlayerStats(1, "dan", "wham");
+        stats.assists = 2;
+        stats.points = 18;
+        stats.penaltyMinutes = 12;
+        viewModel = new PlayerStatsVM(stats, null);
+        viewModel.setSortSelection(StatsSortDialogFragment.StatSortSelection.PenaltyMinutes);
+        playerStats.add(viewModel);
+
+        stats = new PlayerStats(1, "will", "zaon");
+        stats.assists = 1;
+        stats.points = 22;
+        stats.penaltyMinutes = 22;
+        viewModel = new PlayerStatsVM(stats, null);
+        viewModel.setSortSelection(StatsSortDialogFragment.StatSortSelection.PenaltyMinutes);
+        playerStats.add(viewModel);
+
+        stats = new PlayerStats(1, "bob", "aa");
+        stats.assists = 2;
+        stats.points = 3;
+        stats.penaltyMinutes = 12;
+        viewModel = new PlayerStatsVM(stats, null);
+        viewModel.setSortSelection(StatsSortDialogFragment.StatSortSelection.PenaltyMinutes);
+        playerStats.add(viewModel);
+
+        Collections.sort(playerStats);
+
+        Assert.assertEquals("22", playerStats.get(0).getPenaltyMinutes());
+        Assert.assertEquals("zaon", playerStats.get(0).getLastName());
+
+        Assert.assertEquals("12", playerStats.get(1).getPenaltyMinutes());
+        Assert.assertEquals("aa", playerStats.get(1).getLastName());
+
+        Assert.assertEquals("12", playerStats.get(2).getPenaltyMinutes());
+        Assert.assertEquals("wham", playerStats.get(2).getLastName());
+
+        Assert.assertEquals("2", playerStats.get(3).getPenaltyMinutes());
+        Assert.assertEquals("joe", playerStats.get(3).getLastName());
+
+        Assert.assertEquals("0", playerStats.get(4).getPenaltyMinutes());
+        Assert.assertEquals("france", playerStats.get(4).getLastName());
+    }
+
+    @Test
+    public void testSortGamesPlayed() {
+
+        ArrayList<PlayerStatsVM> playerStats = new ArrayList<>();
+
+        PlayerStats stats = new PlayerStats(1, "bob", "joe");
+        stats.assists = 12;
+        stats.points = 33;
+        stats.penaltyMinutes = 2;
+        stats.gamesPlayed = 8;
+        PlayerStatsVM viewModel = new PlayerStatsVM(stats, null);
+        viewModel.setSortSelection(StatsSortDialogFragment.StatSortSelection.GamesPlayed);
+        playerStats.add(viewModel);
+
+        stats = new PlayerStats(1, "jeff", "france");
+        stats.assists = 5;
+        stats.points = 22;
+        stats.penaltyMinutes = 0;
+        stats.gamesPlayed = 8;
+        viewModel = new PlayerStatsVM(stats, null);
+        viewModel.setSortSelection(StatsSortDialogFragment.StatSortSelection.GamesPlayed);
+        playerStats.add(viewModel);
+
+        stats = new PlayerStats(1, "dan", "wham");
+        stats.assists = 2;
+        stats.points = 18;
+        stats.penaltyMinutes = 12;
+        stats.gamesPlayed = 7;
+        viewModel = new PlayerStatsVM(stats, null);
+        viewModel.setSortSelection(StatsSortDialogFragment.StatSortSelection.GamesPlayed);
+        playerStats.add(viewModel);
+
+        stats = new PlayerStats(1, "will", "zaon");
+        stats.assists = 1;
+        stats.points = 22;
+        stats.penaltyMinutes = 22;
+        stats.gamesPlayed = 2;
+        viewModel = new PlayerStatsVM(stats, null);
+        viewModel.setSortSelection(StatsSortDialogFragment.StatSortSelection.GamesPlayed);
+        playerStats.add(viewModel);
+
+        stats = new PlayerStats(1, "bob", "aa");
+        stats.assists = 2;
+        stats.points = 3;
+        stats.penaltyMinutes = 12;
+        stats.gamesPlayed = 8;
+        viewModel = new PlayerStatsVM(stats, null);
+        viewModel.setSortSelection(StatsSortDialogFragment.StatSortSelection.GamesPlayed);
+        playerStats.add(viewModel);
+
+        Collections.sort(playerStats);
+
+        Assert.assertEquals("8", playerStats.get(0).getGamesPlayed());
+        Assert.assertEquals("aa", playerStats.get(0).getLastName());
+
+        Assert.assertEquals("8", playerStats.get(1).getGamesPlayed());
+        Assert.assertEquals("france", playerStats.get(1).getLastName());
+
+        Assert.assertEquals("8", playerStats.get(2).getGamesPlayed());
+        Assert.assertEquals("joe", playerStats.get(2).getLastName());
+
+        Assert.assertEquals("7", playerStats.get(3).getGamesPlayed());
+        Assert.assertEquals("wham", playerStats.get(3).getLastName());
+
+        Assert.assertEquals("2", playerStats.get(4).getGamesPlayed());
+        Assert.assertEquals("zaon", playerStats.get(4).getLastName());
+    }
 }
